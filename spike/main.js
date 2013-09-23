8bab7121f69f0566af2d19c7fd71bfa167c8fffa
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-22 23:53:59 -0400

    started refactoring to prototype -- circle showing

--- spike/main.js
+++ spike/main.js
@@ -2,12 +2,10 @@
 
 // heavily inspired by: http://bl.ocks.org/tomerd/1499279
 
+var xtend = require('xtend');
 var d3 = require('d3');
 
-var el = document.createElement('div');
-document.body.appendChild(el);
-
-var opts = {
+var defaultOpts = {
     size :  250
   , min  :  0
   , max  :  100
@@ -32,95 +30,62 @@ var opts = {
     }
 }
 
+var go = module.exports = Gauge;
+var proto = Gauge.prototype;
 
-var radius = opts.size * 0.9 / 2;
-var cx = opts.size / 2;
-var cy = cx;
-var range = opts.max - opts.min;
+function Gauge (el, opts) {
+  if (!(this instanceof Gauge)) return new Gauge(el, opts);
 
-// TODO: just pass percent i.e. 0.7 - 1.0
-var greenZone = opts.greenZone || {
-    from  :  opts.min + range * 0.75
-  , to    :  opts.min + range * 0.9
-  , color :  '##109618'
+  this.el = el;
+
+  this.opts = xtend(defaultOpts, opts);  
+
+  this.size   =  this.opts.size;
+  this.radius =  this.size * 0.9 / 2;
+  this.cx     =  this.size / 2;
+  this.cy     =  this.cx;
+  this.min    =  this.opts.min;
+  this.max    =  this.opts.max;
+  this.range  =  this.max - this.min;
+  this.outer  =  this.opts.outer;
+
+  // TODO: just pass percent i.e. 0.7 - 1.0
+  this.greenZone = this.opts.greenZone || {
+      from  :  this.min + this.range * 0.75
+    , to    :  this.min + this.range * 0.9
+    , color :  '##109618'
+  }
+
+
+  this.init();
+  this.outerCircle();
+  // continue with prototype
+
+
 }
 
-var gauge = window.g = d3.select(el)
+proto.init = function () {
+  this.gauge = d3.select(this.el)
     .append('svg:svg')
-    .attr('class', 'gauge')
-    .attr('width', opts.size)
-    .attr('height', opts.size)
-    ;
+    .attr('class', 'd3-gauge')
+    .attr('width', this.size)
+    .attr('height', this.size)
+}
 
-  (function outerCircle (gauge, cx, cy, radius, outer) {
-    gauge
-      .append('svg:circle')
-      .attr('cx'            ,  cx)
-      .attr('cy'            ,  cy)
-      .attr('r'             ,  radius)
-      .style('fill'         ,  outer.fill)
-      .style('stroke'       ,  outer.stroke)
-      .style('stroke-width' ,  outer.strokeWidth)
-  })(gauge, cx, cy, radius, opts.outer);
-
-  (function innerCircle (gauge, cx, cy, radius, inner) {
-    gauge
-      .append('svg:circle')
-      .attr('cx'            ,  cx)
-      .attr('cy'            ,  cy)
-      .attr('r'             ,  0.9 * radius)
-      .style('fill'         ,  inner.fill)
-      .style('stroke'       ,  inner.stroke)
-      .style('stroke-width' ,  inner.strokeWidth)
-  })(gauge, cx, cy, radius, opts.inner);
-
-  function toDegrees (value, min, range) {
-    var threeFourthCircle = range * 270;
-    return value / threeFourthCircle - (min / threeFourthCircle + 45);
-  }
-
-  function toRadians (value, min, range) {
-    return toDegrees(value, min, range) * Math.PI / 180;
-  }
-
-  function drawBand (gauge, cx, cy, radius, min, start, end, color) {
-    function transform () {
-      return 'translate(' + cx + ', ' + cy +') rotate(270)';
-    }
-
-    var arc = d3.svg.arc()
-      .startAngle(toRadians(start, min, range))
-      .endAngle(toRadians(end, min, range))
-      .innerRadius(0.65 * radius)
-      .outerRadius(0.85 * radius)
-      ;
-
-    gauge
-      .append('svg:path')
-      .style('fill', color)
-      .attr('d', arc)
-      .attr('transform', transform)
-      
-  }
-
-  (function zones (gauge, cx, cy, radius, min, greenZone) {
-    console.log(greenZone)
-    drawBand(gauge, cx, cy, radius, min, greenZone.from, greenZone.to, greenZone.color);
-  })(gauge, cx, cy, radius, opts.min, greenZone);
+proto.outerCircle = function () {
+  this.gauge
+    .append('svg:circle')
+    .attr('cx'            ,  this.cx)
+    .attr('cy'            ,  this.cy)
+    .attr('r'             ,  this.radius)
+    .style('fill'         ,  this.outer.fill)
+    .style('stroke'       ,  this.outer.stroke)
+    .style('stroke-width' ,  this.outer.strokeWidth)
+}
 
 
-  if (typeof opts.label !== undefined) {
-    (function label (gauge, size, labelOpts) {
-      var fontSize = labelOpts.fontSize || Math.round(size / 9);
-      var halfFontSize = fontSize / 2;
-      gauge
-        .append('svg:text')
-        .attr('x', cx)
-        .attr('y', cy / 2 + halfFontSize)
-        .attr('dy', halfFontSize)
-        .attr('text-anchor', 'middle')
-        .text(labelOpts.text)
-        .style('font-size', fontSize + 'px')
-        .style('fill', labelOpts.fill)
-    })(gauge, opts.size, opts.label);
-  }
+// Test
+
+var el = document.createElement('div');
+document.body.appendChild(el);
+go(el);
