ccc94a3574678fd2a714e9b88c9c7fc24bb7d595
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-24 11:05:46 -0400

    nicely tuned examples

--- example/main.js
+++ example/main.js
@@ -3,19 +3,31 @@
 var gauge = require('../')
   , deepXtend = require('deep-extend')
   , small = require('../defaults/small')
-  , simple = require('../defaults/simple');
+  , simple = require('../defaults/simple')
+  , gauges = [];
 
-function smallGauge () {
-  var smallEl = document.createElement('div');
-  document.body.appendChild(smallEl);
-  gauge(smallEl, deepXtend(small, { label: { text: 'Proc Mem' } }));
+function createGauge (opts) {
+  var el = document.createElement('div');
+  el.setAttribute('class', 'gauge-container');
+  document.body.appendChild(el);
+  var g = gauge(el, opts);
+  g.currentValue = g._range / 2;
+  gauges.push(g);
 }
 
-function simpleGauge () {
-  var simpleEl = document.createElement('div');
-  document.body.appendChild(simpleEl);
-  gauge(simpleEl, { label: { text: 'Main Mem' } });
+function getRandomNextValue(gauge) {
+  gauge.currentValue += (Math.random() - 0.5) * gauge._range / 10; 
+  return gauge.currentValue;
 }
 
-simpleGauge();
-smallGauge();
+function updateGauges() {
+  gauges.forEach(function (gauge) {
+    gauge.redraw(getRandomNextValue(gauge));
+  });
+}
+
+// TODO: fix deep extend - currently messes with original
+createGauge({ label: { text: 'Main Mem' } });
+createGauge(deepXtend(small, { label: { text: 'Proc Mem' } }));
+
+setInterval(updateGauges, 500);
